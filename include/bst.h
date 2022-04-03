92c36f7f9031c044c6ea055229dffd6e33998b64
alirezatabatabaeian <alireza2541@aut.ac.ir>
2022-04-03 18:13:15 +0430

    3: Cout & Get_root

--- include/bst.h
+++ include/bst.h
@@ -1,28 +1,32 @@
 #ifndef BST_H
 #define BST_H
-
+#include <compare>
+#include <iomanip>
+#include <iostream>
 class BST {
 public:
-
     class Node {
     public:
         Node(int value, Node* left, Node* right);
         Node();
         Node(const Node& node);
+        std::partial_ordering operator<=>(int _value) const;
+        bool operator==(int _value) const;
+        friend std::ostream& operator<<(std::ostream& cout, const BST::Node& node);
 
         int value;
         Node* left;
         Node* right;
     };
 
-    // Node*& get_root();
-    // void bfs(std::function<void(Node*& node)> func);
-    // size_t length();
-    // bool add_node(int value);
-    // Node** find_node(int value);
-    // Node** find_parrent(int value);
-    // Node** find_successor(int value);
-    // bool delete_node(int value);
+    Node*& get_root() { return root; }
+    void bfs(std::function<void(Node*& node)> func);
+    //  size_t length();
+    //  bool add_node(int value);
+    //  Node** find_node(int value);
+    //  Node** find_parrent(int value);
+    //  Node** find_successor(int value);
+    //  bool delete_node(int value);
 
 private:
     Node* root;
--- src/bst.cpp
+++ src/bst.cpp
@@ -21,3 +21,21 @@ BST::Node::Node(const Node& node)
 {
 }
 
+std::ostream& operator<<(std::ostream& cout, const BST::Node& node)
+{
+    cout << std::setw(20) << std::left << &node << "=> "
+         << "Value:" << std::setw(5) << std::left << node.value
+         << "Left:" << std::setw(20) << std::left << node.left
+         << "Right:" << std::setw(20) << std::left << node.right;
+    return cout;
+}
+
+std::partial_ordering BST::Node::operator<=>(int _value) const { return value <=> _value; }
+
+bool BST::Node::operator==(int _value) const { return value == _value; }
+
+void BST::bfs(std::function<void(Node*& node)> func){
+    while(node != ){
+
+    }
+}
