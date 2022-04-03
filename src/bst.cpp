57e19554730535c31df87b50d94e912c5dbed832
alirezatabatabaeian <alireza2541@aut.ac.ir>
2022-04-03 16:27:55 +0430

    2: Constructor of Node Class

--- src/bst.cpp
+++ src/bst.cpp
@@ -1,1 +1,23 @@
 #include "bst.h"
+
+BST::Node::Node(int _value, Node* _left, Node* _right)
+    : value { _value }
+    , left { _left }
+    , right { _right }
+{
+}
+
+BST::Node::Node()
+    : value {}
+    , left { nullptr }
+    , right { nullptr }
+{
+}
+
+BST::Node::Node(const Node& node)
+    : value { node.value }
+    , left { node.left }
+    , right { node.right }
+{
+}
+
